902a69a778be90a0a35b3c5d8d5b0f4e8f1613a0
BEX-0 <126423309+BEX-0@users.noreply.github.com>
2023-07-12 16:42:54 -0500

    simplified showDateTime function by putting time zone names in select values

--- src/script.js
+++ src/script.js
@@ -1,23 +1,8 @@
-let tokyoTime = moment.tz("Asia/Tokyo")
-.format("dddd, MMMM D, YYYY h:m A");
-
-let sydneyTime = moment.tz("Austrailia/Sydney")
-.format("dddd, MMMM D, YYYY h:m A");
-
-let parisTime = moment.tz("France/Paris")
-.format("dddd, MMMM D, YYYY h:m A");
-
 function showDateTime(event) {
-    if (event.target.value === "tokyo") {
-      alert(`It is ${tokyoTime} in Asia/Tokyo`);
-    }
-    if (event.target.value === "sydney") {
-      alert(`It is ${sydneyTime} in Austrailia/Sydney`);
-    }
-    if (event.target.value === "paris") {
-      alert(`It is ${parisTime} in France/Paris`);
-    }
-  }
+    let timezone = event.target.value;
+    let currentDateTime = moment.tz(timezone).format("dddd, MMMM D, YYYY h:m A");
+        alert(`It is ${currentDateTime} in ${timezone}`);
+}
 
 let countrySelect = document.querySelector("#countries");
 countrySelect.addEventListener("change", showDateTime);
