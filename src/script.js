26e549b5c85f13d352d66103515a64ab9d753e51
BEX-0 <126423309+BEX-0@users.noreply.github.com>
2023-07-13 16:42:18 -0500

    added function to update the placeholder cities when file is opened

--- src/script.js
+++ src/script.js
@@ -1,3 +1,25 @@
+function updatePlaceholders() {
+    let newYork = document.querySelector("#new-york");
+    let newYorkData = moment.tz("America/New_York");
+    let newYorkDate = newYork.querySelector(".date");
+    newYorkDate.innerHTML = newYorkData.format("dddd MMMM D, YYYY");
+
+    let newYorkTime = newYork.querySelector(".time");
+    newYorkTime.innerHTML = newYorkData.format(`h:mm:ss [<small>] A[</small>]`);
+
+
+    let amsterdam = document.querySelector("#amsterdam");
+    let amsterdamData = moment.tz("Europe/Amsterdam");
+    let amsterdamDate = amsterdam.querySelector(".date");
+    amsterdamDate.innerHTML = amsterdamData.format("dddd MMMM D, YYYY");
+
+    let amsterdamTime = amsterdam.querySelector(".time");
+    amsterdamTime.innerHTML =amsterdamData.format(`h:mm:ss [<small>] A[</small>]`);
+}
+
+updatePlaceholders();
+setInterval(updatePlaceholders, 1000);
+
 function showDateTime(event) {
     let timezone = event.target.value;
     let currentData = moment().tz(timezone);
@@ -16,6 +38,3 @@ function showDateTime(event) {
 
 let citySelect = document.querySelector("#cities");
 citySelect.addEventListener("change", showDateTime);
-
-showDateTime();
-setInterval(showDateTime, 1000);
